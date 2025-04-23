d4d6b20b7c071008c8bd4d933894a60135fa005e
rodainaelsawy <rodainaahmed711@gmail.com>
2025-04-23 11:20:15 +0200

    parse done

--- src/parser.c
+++ src/parser.c
@@ -16,6 +16,11 @@ int parse_program(const char *path, instruction_t **out_code) {
         instruction_t inst = {0};
         if (strcmp(tok, "assign") == 0) inst.type = INST_ASSIGN;
         else if (strcmp(tok, "print") == 0) inst.type = INST_PRINT;
+        else if (strcmp(tok, "writeFile") == 0) inst.type = INST_WRITE_FILE;
+        else if (strcmp(tok, "readFile") == 0) inst.type = INST_READ_FILE;
+        else if (strcmp(tok, "printFromTo") == 0) inst.type = INST_PRINT_FROM_TO;
+        else if (strcmp(tok, "semWait") == 0) inst.type = INST_SEM_WAIT;
+        else if (strcmp(tok, "semSignal") == 0) inst.type = INST_SEM_SIGNAL;
         // ... handle all cases ...
         // then read args: arg1 = strtok(NULL), arg2 = strtok(NULL)
         char *a1 = strtok(NULL, " \n");
